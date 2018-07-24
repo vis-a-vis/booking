742e2d0c7cf88d3be85cab24dd8e715edd019959
SamMahani <sam.amirmahani@gmail.com>
2018-07-24 15:29:40 -0700

    Implement new get method

--- server/server.js
+++ server/server.js
@@ -1,12 +1,22 @@
+// IMPORT/////////////////////////////////////////////////////////////
+
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3002;
 
+// APP METHODS///////////////////////////////////////////////////////
+
 // look up morgan
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, '../public')));
+
+// GET METHODS///////////////////////////////////////////////////////
+
+app.get('/rooms/:roomId', (req, res) => {});
+
+// APP METHODS///////////////////////////////////////////////////////
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
