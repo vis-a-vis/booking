f38c84351b104ffe01a6fddeca59512a8677964e
SamMahani <sam.amirmahani@gmail.com>
2018-07-30 17:14:20 -0700

    Implement data generator skeleton

--- db/dataGenerator.js
+++ db/dataGenerator.js
@@ -1,42 +1,63 @@
-const listings = [
-  {
-    listingId: 0,
-    price: 195,
-    minStay: 1,
-    lastUpdate: new Date('April 12, 1997'),
-    bookings: [],
-    reviewStars: 1,
-    reviewCount: 2034,
-  },
-  {
-    listingId: 1,
-    price: 90,
-    minStay: 1,
-    lastUpdate: new Date('July 17, 2018'),
-    bookings: [
-      [new Date('July 4, 2018'), new Date('July 8, 2018')],
-      [new Date('July 19, 2018'), new Date('July 22, 2018')],
-      [new Date('July 29, 2018'), new Date('August 7, 2018')],
-    ],
-    reviewStars: 4,
-    reviewCount: 18,
-  },
-  {
-    listingId: 2,
-    price: 135,
-    minStay: 1,
-    lastUpdate: new Date('December 17, 2017'),
-    bookings: [
-      [new Date('June 4, 2018'), new Date('June 8, 2018')],
-      [new Date('June 19, 2018'), new Date('June 22, 2018')],
-      [new Date('June 29, 2018'), new Date('July 7, 2018')],
-    ],
-    reviewStars: 2,
-    reviewCount: 102,
-  },
-];
+const faker = require('faker');
+
+const randNumArrays = [];
+const addNums = () => {
+  for (let i = 0; i < 6; i += 1) {
+    const tempArr = [];
+    while (tempArr.length < 8) {
+      const randNum = Math.floor(Math.random() * 30) + 1;
+      if (!tempArr.includes(randNum)) {
+        tempArr.push(randNum);
+      }
+    }
+    tempArr.sort((a, b) => a - b);
+    randNumArrays.push(tempArr);
+  }
+};
+addNums();
+
+const listingObj = {
+  listingId: 1,
+  price: Math.floor(faker.commerce.price()),
+  cleaningFee: Math.floor(faker.commerce.price()),
+  minStay: Math.floor(Math.random() * 3) + 1,
+  maxGuests: Math.floor(Math.random() * 5) + 1,
+  lastUpdate: faker.date.recent(90),
+  bookings: [
+    [new Date(`2018-07-${randNumArrays[0][0]}`), new Date(`2018-07-${randNumArrays[0][1]}`)],
+    [new Date(`2018-07-${randNumArrays[0][2]}`), new Date(`2018-07-${randNumArrays[0][3]}`)],
+    [new Date(`2018-07-${randNumArrays[0][4]}`), new Date(`2018-07-${randNumArrays[0][5]}`)],
+    [new Date(`2018-07-${randNumArrays[0][6]}`), new Date(`2018-07-${randNumArrays[0][7]}`)],
+
+    [new Date(`2018-08-${randNumArrays[1][0]}`), new Date(`2018-08-${randNumArrays[1][1]}`)],
+    [new Date(`2018-08-${randNumArrays[1][2]}`), new Date(`2018-08-${randNumArrays[1][3]}`)],
+    [new Date(`2018-08-${randNumArrays[1][4]}`), new Date(`2018-08-${randNumArrays[1][5]}`)],
+    [new Date(`2018-08-${randNumArrays[1][6]}`), new Date(`2018-08-${randNumArrays[1][7]}`)],
+
+    [new Date(`2018-09-${randNumArrays[2][0]}`), new Date(`2018-09-${randNumArrays[2][1]}`)],
+    [new Date(`2018-09-${randNumArrays[2][2]}`), new Date(`2018-09-${randNumArrays[2][3]}`)],
+    [new Date(`2018-09-${randNumArrays[2][4]}`), new Date(`2018-09-${randNumArrays[2][5]}`)],
+    [new Date(`2018-09-${randNumArrays[2][6]}`), new Date(`2018-09-${randNumArrays[2][7]}`)],
+
+    [new Date(`2018-10-${randNumArrays[3][0]}`), new Date(`2018-10-${randNumArrays[3][1]}`)],
+    [new Date(`2018-10-${randNumArrays[3][2]}`), new Date(`2018-10-${randNumArrays[3][3]}`)],
+    [new Date(`2018-10-${randNumArrays[3][4]}`), new Date(`2018-10-${randNumArrays[3][5]}`)],
+    [new Date(`2018-10-${randNumArrays[3][6]}`), new Date(`2018-10-${randNumArrays[3][7]}`)],
+
+    [new Date(`2018-11-${randNumArrays[4][0]}`), new Date(`2018-11-${randNumArrays[4][1]}`)],
+    [new Date(`2018-11-${randNumArrays[4][2]}`), new Date(`2018-11-${randNumArrays[4][3]}`)],
+    [new Date(`2018-11-${randNumArrays[4][4]}`), new Date(`2018-11-${randNumArrays[4][5]}`)],
+    [new Date(`2018-11-${randNumArrays[4][6]}`), new Date(`2018-11-${randNumArrays[4][7]}`)],
+
+    [new Date(`2018-12-${randNumArrays[5][0]}`), new Date(`2018-12-${randNumArrays[5][1]}`)],
+    [new Date(`2018-12-${randNumArrays[5][2]}`), new Date(`2018-12-${randNumArrays[5][3]}`)],
+    [new Date(`2018-12-${randNumArrays[5][4]}`), new Date(`2018-12-${randNumArrays[5][5]}`)],
+    [new Date(`2018-12-${randNumArrays[5][6]}`), new Date(`2018-12-${randNumArrays[5][7]}`)],
+  ],
+  reviewStars: Math.floor(Math.random() * 5) + 1,
+  reviewCount: faker.random.number(),
+};
 
 module.exports = {
-  listings: listings,
-  test: function() {console.log('JLAFJEKWF')},
-}
+  listings: listingObj,
+};
