14eed4ccf8c9c5e8fdcacdbf9c315680ed6c842f
SamMahani <sam.amirmahani@gmail.com>
2018-07-31 14:18:07 -0700

    Seed database

--- db/dataSeeder.js
+++ db/dataSeeder.js
@@ -1,3 +1,52 @@
+const mongoose = require('mongoose');
 const { listings } = require('./dataGenerator');
 
-console.log(listings);
+mongoose.connect('mongodb://localhost:27017');
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  let listingSchema = new mongoose.Schema({
+    listingId: { type: Number, unique: true },
+    price: Number,
+    cleaningFee: Number,
+    minStay: Number,
+    maxGuests: Number,
+    lastUpdate: Date,
+    bookings: [
+      [Date, Date],
+      [Date, Date],
+      [Date, Date],
+      [Date, Date],
+      [Date, Date],
+      [Date, Date],
+      [Date, Date],
+      [Date, Date],
+      [Date, Date],
+      [Date, Date],
+      [Date, Date],
+      [Date, Date],
+      [Date, Date],
+      [Date, Date],
+      [Date, Date],
+      [Date, Date],
+      [Date, Date],
+      [Date, Date],
+      [Date, Date],
+      [Date, Date],
+      [Date, Date],
+      [Date, Date],
+      [Date, Date],
+      [Date, Date],
+    ],
+    reviewStars: Number,
+    reviewCount: Number,
+  });
+  let Listing = mongoose.model('Listing', listingSchema);
+  Listing.insertMany(listings)
+    .then(console.log('listings have been added to db'))
+    .catch(console.log('error adding listings'));
+
+  module.exports = {
+    Listing: Listing,
+  };
+});
