d23e343444c462b7ecf7be18ff74ab59280ecd1b
SamMahani <sam.amirmahani@gmail.com>
2018-08-01 00:30:17 -0700

    Add click handlers for plus/minus buttons

--- client/src/components/guestComponent.jsx
+++ client/src/components/guestComponent.jsx
@@ -5,33 +5,108 @@ class guestDropdown extends React.Component {
     super(props);
     this.state = {
       open: false,
+      adults: 0,
+      children: 0,
+      infants: 0,
     };
+    this.handlePlusClick = this.handlePlusClick.bind(this);
+    this.handleMinusClick = this.handleMinusClick.bind(this);
   }
 
+
+  //contain 0 to maxGuests
+  handlePlusClick(string) {
+    const { adults, children, infants } = this.state;
+    if (string === 'adults') {
+      this.setState({ adults: adults + 1 });
+    } else if (string === 'children') {
+      this.setState({ children: children + 1 });
+    } else if (string === 'infants') {
+      this.setState({ infants: infants + 1 });
+    }
+  }
+
+  handleMinusClick(string) {
+    const { adults, children, infants } = this.state;
+    if (string === 'adults') {
+      this.setState({ adults: adults - 1 });
+    } else if (string === 'children') {
+      this.setState({ children: children - 1 });
+    } else if (string === 'infants') {
+      this.setState({ infants: infants - 1 });
+    }
+  }
+
+
   render() {
+    const { adults, children, infants } = this.state;
     return (
     <div>
       <nav>
         <p>guests</p>
         <ul>
+
+
           <li>
-            <a href="#">Adults</a>
-            <button>+</button>
-            <a href="">##</a>
-            <button>-</button>
-            </li>
-          <li>
-            <a href="#">Children</a>
-            <button>+</button>
-            <a href="">##</a>
-            <button>-</button>
+            <a href="adults">Adults</a>
+            <button
+              type="button"
+              onClick={() => this.handleMinusClick('adults')}
+            >
+             -
+            </button>
+            <span>
+              { adults }
+            </span>
+            <button
+              type="button"
+              onClick={() => this.handlePlusClick('adults')}
+            >
+             +
+            </button>
           </li>
+
+
           <li>
-            <a href="#">Infants</a>
-            <button>+</button>
-            <a href="">##</a>
-            <button>-</button>
+            <a href="children">Children</a>
+            <button
+              type="button"
+              onClick={() => this.handleMinusClick('children')}
+            >
+             -
+            </button>
+            <span>
+              { children }
+            </span>
+            <button
+              type="button"
+              onClick={() => this.handlePlusClick('children')}
+            >
+             +
+            </button>
           </li>
+
+
+          <li>
+            <a href="infants">Infants</a>
+            <button
+              type="button"
+              onClick={() => this.handleMinusClick('infants')}
+            >
+             -
+            </button>
+            <span>
+              { infants }
+            </span>
+            <button
+              type="button"
+              onClick={() => this.handlePlusClick('infants')}
+            >
+             +
+            </button>
+          </li>
+
+
         </ul>
       </nav>
     </div>
