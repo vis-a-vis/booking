a62785901d331c49a1224d4629581f019daec3f6
SamMahani <sam.amirmahani@gmail.com>
2018-08-04 17:25:47 -0700

    Add event tests

--- tests/GuestComponent.test.js
+++ tests/GuestComponent.test.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import GuestComponent from '../client/src/components/GuestComponent';
-import { shallow } from 'enzyme';
+import { shallow, mount, render } from 'enzyme';
 
-describe('GuestComponent', () => {
-  it('starts with an adult count of 1', () => {
-    const wrapper = shallow(<GuestComponent />);
-    const adultsState = wrapper.state().adults;
-    expect(adultsState).toEqual(1);
+
+describe('Guest Component', () => {
+  const wrapper = shallow(<GuestComponent />);
+  // STATE TESTS //////////////////////////////////////////////////////
+
+  it('should start with correct state key-value pairs', () => {
+    const obj = {
+      open: false,
+      adults: 1,
+      children: 0,
+      infants: 0,
+    };
+    expect(wrapper.state()).toEqual(obj);
+  });
+
+  // RENDER TESTS /////////////////////////////////////////////////////
+
+  it('should render without throwing error', () => {
+    expect((wrapper).find('.guestDropdownMain').exists()).toBe(true);
+  });
+
+  it('should render three drowDown items', () => {
+    expect((wrapper).find('.dropdownItem').length).toEqual(3);
+  });
+
+  // EVENT TESTS /////////////////////////////////////////////////////
+
+  it('should respond to change event and change the state of the Guest Component', () => {
+    wrapper.find('.dropDownItems').simulate('click');
+    expect(wrapper.state().open).toBe(true);
   });
 });
-
-
-//use find for actual html
