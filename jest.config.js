f8a434f35a8b08be72b4a210f9e236d40d5f49d8
SamMahani <sam.amirmahani@gmail.com>
2018-09-03 12:57:03 -0700

    Update jest config file

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
-  setupFiles: ['<rootDir>/tests/setup.js'],
+  setupFiles: ['<rootDir>/jest.setup.js'],
+  roots: ['<rootDir>/__tests__'],
   verbose: true,
+  collectCoverage: true,
+  coverageDirectory: '<rootDir>/__coverage__',
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
     "\\.(css|less)$": "<rootDir>/node_modules/jest-css-modules",
